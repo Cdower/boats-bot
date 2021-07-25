1296466f7b7c3e09b573089a06d4f608f6010f7b
Chris Dower <cliffjumpers57@gmail.com>
2021-07-24 22:45:21 -0700

    fix warnings

--- main.py
+++ main.py
@@ -39,51 +39,51 @@ class BoatsBot:
       return to_boat
 
   def update_boats(self, author, count: int = 1) -> None:
-    if author.id in db.keys():
-      db[author.id] += count
+    if author.id in self.db.keys():
+      self.db[author.id] += count
     else:
-      db[author.id] = count
+      self.db[author.id] = count
 
   def __init__(self, db: str = 'boatsbot.db') -> None:
     self.client = discord.Client()
     self.db = None
     try:
       self.db = sqlite3.connect(db)
-    except Error as e:
+    except Exception as e:
       print(e)
     self.__init_db()
 
 
   def start(self, token: str = os.getenv('TOKEN')) -> None:
-    client.run(token)
+    self.client.run(token)
 
   @client.event
   async def on_ready(self) -> None:
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(self.client))
 
   @client.event
   async def on_message(self, message):
     bizz_count = 0
     buzz_count = 0
 
-    if message.author == client.user:
+    if message.author == self.client.user:
       return
 
     if message.content.startswith('$leaderboard'):
       await message.channel.send('All-Time boat count:')
 
     elif message.content.startswith('$boats'):
-      to_boat = get_to_boat(message.author)
+      to_boat = self.get_to_boat(message.author)
       await message.channel.send(f'You are {to_boat} to boat')
 
     bizz_count = message.content.count('five') + message.content.count('5')
     buzz_count = message.content.count('seven') + message.content.count('7')
     boats_count = bizz_count + buzz_count
-    to_boat = get_to_boat(message.author)
+    to_boat = self.get_to_boat(message.author)
 
     if boats_count > 0:
       print(f"updating count with: {boats_count}")
-      update_boats(message.author, count=boats_count)
+      self.update_boats(message.author, count=boats_count)
       bizz_word = "bizzes" if bizz_count != 1 else "bizz"
       buzz_word = "buzzes" if buzz_count != 1 else "buzz"
       boats_string = "" if to_boat - boats_count > 0 else f"you're boats @{message.author}, "
